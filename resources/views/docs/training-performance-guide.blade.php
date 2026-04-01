ebebc7bf84d3c3459625f62321bd189c573f8f92
Togrul Calalli <t.calalli@ptx.gov.az>
2026-04-02 00:16:58 +0400

    route 2

--- resources/views/docs/training-performance-guide.blade.php
+++ resources/views/docs/training-performance-guide.blade.php
@@ -158,7 +158,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HR Architect Documentation</title>
-    @vite(['resources/css/font.css'])
+    @vite(['resources/css/app.css'])
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet">
     <style>
